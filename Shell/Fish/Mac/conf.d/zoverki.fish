f8233fa3029b63c6498187995199a86747b48d2b
ZoveRKI <168569997+ZoveRKI@users.noreply.github.com>
2024-11-28 18:30:40 +0900

    Create zoverki.fish

---
+++ Shell/Fish/Mac/conf.d/zoverki.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+status is-interactive; or return
+
+# export PATH="$PATH:/Users/shi.yu/jdk-21.0.3-full.jdk/bin"
+# export JAVA_HOME="/Users/shi.yu/jdk-21.0.3-full.jdk"
+# export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+# export PATH="$PATH:/Users/shi.yu/git_work/jbc-itk/jbc-itk/vendor/bin/sail"
+# alias sail='./vendor/bin/sail up'
